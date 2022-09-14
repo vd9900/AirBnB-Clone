da1e4120ab62816276bde0dde01445ea510275e7
Vinith <vinithdevadiga7@gmail.com>
2022-09-14 23:13:10 +0530

    schema added

--- Sever/Schemas/schema.js
+++ Sever/Schemas/schema.js
@@ -1,1 +1,40 @@
-const mongoose = require("mo")
+const mongoose = require("mongoose");
+
+const userDetailSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    userName: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    password: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    dob: {
+        type: Date,
+    },
+    country: {
+        type: String
+    },
+    city: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    profile: {
+        data: Buffer,
+        contentType: String
+    },
+    time: {
+        type: Date,
+        default: Date.now
+    }
+
+})
