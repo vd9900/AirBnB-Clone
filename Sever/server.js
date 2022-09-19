bb8c4eda439614b83388016f0c192d2901545930
Vinith <vinithdevadiga7@gmail.com>
2022-09-19 19:49:48 +0530

    router updated

--- Sever/server.js
+++ Sever/server.js
@@ -7,21 +7,22 @@ const appRouter = express.Router();
 // Middlewares
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
-app.use("/",appRouter)
+app.use("/", appRouter)
 
-app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname,'../views/index.html'));
-  });
-  app.get('/home', function(req,res){
-    res.sendFile(path.join(__dirname,'../views/home.html'));
-  });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../views/index.html'));
+});
+app.get('/home', function (req, res) {
+  res.sendFile(path.join(__dirname, '../views/home.html'));
+});
 // home page
 // appRouter.route("/").get(homeGET)
 
 // // SignUp & Login page
 // appRouter.route("/login").get(loginGET).post(loginPOST)
 // appRouter.route("/signin").post(signinPOST)
-app.post("/signin",(req,res)=>{
+app.post("/signin", (req, res) => {
+  
   res.redirect("/home")
 })
 
@@ -34,7 +35,7 @@ app.post("/signin",(req,res)=>{
 // Admin page
 // appRouter.route("/Admin").get(adminGET)
 
-function signinPOST(req,res){
-    res.send(req.body);
+function signinPOST(req, res) {
+  res.send(req.body);
 }
 app.listen(4000)
