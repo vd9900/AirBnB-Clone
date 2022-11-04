9b96721c545010ece73961ed5b2d16106ddcee2f
Vinith <vinithdevadiga7@gmail.com>
2022-11-04 22:35:30 +0530

    updated

--- contorller/property.js
+++ contorller/property.js
@@ -1,19 +1,3 @@
-const path = require('path')
 
 
-async function  propertyGET(req, res) {
-    if (req.session.isAuth) {
-        // current clicked properity
-        if (await req.session.clickedpro) {
-            delete req.session.clickedpro
-        }
-        req.session.clickedpro = req.params.id
-
-        console.log(req.params.id,"hlel");
-        console.log(req.session.clickedpro,"bhai sab");
-        res.sendFile(path.join(__dirname, "../views/product.html"))
-    } else {
-        res.redirect("/login")
-    }
-}
-module.exports = { propertyGET }
+// module.exports = Router;
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const moongose = require("mongoose")
 const path = require("path");
-const bcrypt = require("bcrypt")
 const multer = require('multer');
 var session = require('express-session')
 const { default: mongoose } = require("mongoose");
@@ -12,16 +11,17 @@ const appRouter = express.Router();
 const { signinPOST } = require("./contorller/signin")
 const { loginGET, loginPOST } = require("./contorller/login")
 const { homeGET } = require("./contorller/home")
-const { propertyGET } = require("./contorller/property")
 const { bookingconfGET, bookingconfPOST } = require("./contorller/bookingconf")
 const { bookingconformedGET } = require("./contorller/bookingconformed")
 const { mybookingsGET } = require("./contorller/mybooking")
 const { mybookingsdetailsGET } = require("./contorller/mybookingdetails")
 const { helpGET } = require("./contorller/help")
 const { logoutGET } = require("./contorller/logout")
-const { UserDetail, HostedRoomDetails, BookedroomDetails,ReviewDetails } = require("./Schemas/schema")
+const { UserDetail, HostedRoomDetails, BookedroomDetails, ReviewDetails } = require("./Schemas/schema")
 
-const hosterRoute = require("./contorller/hoster")
+const propertyRoute = require("./contorller/property")
+const hosterRoute = require("./contorller/hoster");
+const { nextTick } = require("process");
 //session
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
@@ -71,10 +71,33 @@ appRouter.route("/home").get(homeGET)
 
 //Hoster page
 
-appRouter.use("/hoster",hosterRoute)
+appRouter.use("/hoster", hosterRoute)
 
 //Product page
-appRouter.route("/property/:id").get(propertyGET)
+appRouter.get("/property/:id", async function propertyGET(req, res) {
+  if (req.session.isAuth) {
+    // current clicked properity
+
+    if (await req.session.clickedpro) {
+      delete req.session.clickedpro
+    }
+    req.session.clickedpro = req.params.id
+    console.log(req.session.clickedpro)
+    console.log(req.params.id);
+    console.log(req.originalUrl)
+    res.sendFile(path.join(__dirname, "./views/product.html"))
+  } else {
+    res.redirect("/login")
+  }
+
+})
+
+appRouter.get("/fetchproperty", async (req, res) => {
+  console.log(req.session);
+  console.log(req.session.clickedpro)
+  const result = await HostedRoomDetails.findOne({ propertyId: req.session.clickedpro}, {})
+  res.json(result)
+})
 
 // my booking conformation  page
 appRouter.route("/property/bookingconf").get(bookingconfGET).post(bookingconfPOST)
@@ -93,7 +116,7 @@ appRouter.route("/mybooking/:id").get(mybookingsdetailsGET)
 app.post("/rating", (req, res) => {
   console.log(req.body);
   const NewreviewDetail = new ReviewDetails({
-    
+
   })
 
   res.send(`<h3>Thanks for your review🥰</h3>
@@ -114,12 +137,9 @@ app.get("/fetchproperties", async (req, res) => {
   res.json(result)
 
 })
-app.get("/fetchproperty", async (req, res) => {
-  console.log(req.session.clickedpro)
-  const result = await HostedRoomDetails.findOne({ propertyId: req.params.id }, {})
-  res.json(result)
-})
+
 app.get("/fetchbookedRoom", async (req, res) => {
+  console.log(req.session);
   const result = await HostedRoomDetails.findOne({ propertyId: req.session.clickedpro }, {})
   req.session.bookedRoom.roomDetails = {
     propertyName: result.propertyName,
